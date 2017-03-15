5a2d5ff7b3a3afd0e891bfbe16380791f79fcee0
Jason Reeder <info@jasonreeder.com>
2017-03-15 14:31:47 -0700

    Add to form

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -5,13 +5,76 @@ class SessionForm extends React.Component {
   constructor (props) {
     super(props);
     this.state = { username: "", password: "" };
-    /* this.handleSubmit = this.handleSubmit.bind(this);*/
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.redirectIfLoggedIn();
+  }
+
+  redirectIfLoggedIn() {
+    if (this.props.loggedIn) {
+      this.props.router.push('/');
+    }
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = this.state;
+    this.props.processForm({user});
+  }
+
+  navLink() {
+    if (this.props.formType === 'login') {
+      return <Link to='/join'>join instead</Link>;
+    } else {
+      return <Link to='/signin'>sign in instead</Link>;
+    }
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, idx) => (
+          <li key={`error-${idx}`}>
+            {error}
+          </li>
+         ))}
+      </ul>
+    );
   }
 
   render() {
     return(
       <div>
-        Hello
+        <form onSubmit={this.handleSubmit}>
+          <h1>Sign In / Join</h1>
+          Please {this.props.formType} or {this.navLink()}
+          {this.renderErrors()}
+          <div className="login-form">
+            <label>
+              <input
+                type='text'
+                value={this.state.username}
+                onChange={this.update('username')}
+                className="login-input"
+              />
+            </label>
+              <input
+                type='password'
+                value={this.state.password}
+                onChange={this.update('password')}
+                className="login-input"
+              />
+          </div>
+          <input type='submit' value='Submit' />
+        </form>
       </div>
     );
   }
