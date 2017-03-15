6a0bc755ed75d3f99fc3c8aa0f6240b67f8e8081
Jason Reeder <info@jasonreeder.com>
2017-03-15 15:04:32 -0700

    Prevent signed in users from accessing sign in

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -5,14 +5,28 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
 import SessionFormContainer from './session/session_form_container';
 
-const Root = ({ store }) => (
-  <Provider store={ store }>
-    <Router history={ hashHistory }>
-      <Route path="/" component={ App } />
-        <Route path="/signin" component={ SessionFormContainer } />
-        <Route path="/join" component={ SessionFormContainer } />
-    </Router>
-  </Provider>
-);
+const Root = ({ store }) => {
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  };
+
+  return (
+    <Provider store={ store }>
+      <Router history={ hashHistory }>
+        <Route path="/" component={ App } />
+        <Route
+          path="/signin"
+          component={ SessionFormContainer }
+          onEnter={ _redirectIfLoggedIn }
+        />
+          <Route path="/join" component={ SessionFormContainer } />
+      </Router>
+    </Provider>
+  );
+};
 
 export default Root;
