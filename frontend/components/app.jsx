baefbfb08639bdced32ae0dc185cee61a8fc48c3
Jason Reeder <info@jasonreeder.com>
2017-03-15 15:31:41 -0700

    Add splash container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import GreetingContainer from './greeting/greeting_container';
-import Splash from './splash/splash';
+import SplashContainer from './splash/splash_container';
 
 const App = ({ children }) => (
   <section className='app-container'>
-    <Splash />
+    <SplashContainer />
   </section>
 );
 
