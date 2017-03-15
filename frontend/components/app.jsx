dda71c37e464e8cd57c55c483fdf5a0e7c56b088
Jason Reeder <info@jasonreeder.com>
2017-03-14 17:37:14 -0700

    Add styling and splash content

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
 const App = () => (
-  <div>
-    <p>Mr. Watson, come here. I want you.</p>
-  </div>
+  <section className='app-container'>
+    <header className='auth-header'>
+      <ul className='brand'>
+        <li>[logo]</li>
+        <li>smack</li>
+      </ul>
+      <ul className='nav'>
+        <li>Product</li>
+        <li>Sign in</li>
+      </ul>
+    </header>
+    <section className='splash'>
+      <div className='splash-content'>
+        <h1>
+          Now you're<br />talking<strong>*</strong>
+        </h1>
+        <p>
+          A chat app, inspired by Slack
+        </p>
+      </div>
+    </section>
+  </section>
 );
 
 export default App;
