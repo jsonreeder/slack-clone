5d56e476ae756675f96640d4a89ff919ab1d607c
Jason Reeder <info@jasonreeder.com>
2017-03-15 15:24:05 -0700

    Move out splash content

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,23 +2,14 @@ import React from 'react';
 
 import GreetingContainer from './greeting/greeting_container';
 import SplashHeader from './splash/slash_header';
+import SplashContent from './splash/splash_content';
 
 const App = ({ children }) => (
   <section className='app-container'>
     <SplashHeader />
     <GreetingContainer />
     { children }
-
-    <section className='splash'>
-      <div className='splash-content'>
-        <h1>
-          Now you're<br />talking<strong>*</strong>
-        </h1>
-        <p>
-          A chat app, inspired by Slack
-        </p>
-      </div>
-    </section>
+    <SplashContent />
   </section>
 );
 
---
+++ frontend/components/splash/splash_content.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SplashContent = () => (
+  <section className='splash'>
+    <div className='splash-content'>
+      <h1>
+        Now you're<br />talking<strong>*</strong>
+      </h1>
+      <p>
+        A chat app, inspired by Slack
+      </p>
+    </div>
+  </section>
+);
+
+export default SplashContent;
