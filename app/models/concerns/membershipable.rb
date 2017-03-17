c3146e55622d7f9fde11ae87d09f69753927bdbf
Jason Reeder <info@jasonreeder.com>
2017-03-17 16:02:53 -0700

    Add associations

--- app/models/concerns/membershipable.rb
+++ app/models/concerns/membershipable.rb
@@ -3,6 +3,10 @@ module Membershipable
 
   included do
     has_many :memberships, as: :membershipable
+    has_many :forums,
+             through: :memberships,
+             source: :forum
+
   end
 
   def join(forum_id)
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -14,4 +14,5 @@ class Membership < ApplicationRecord
   validates :forum_id, :membershipable_id, :membershipable_type, presence: true
   validates :forum_id, uniqueness: { scope: [:membershipable] }
   belongs_to :membershipable, polymorphic: true
+  belongs_to :forum
 end
