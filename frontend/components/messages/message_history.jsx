2675284212d1b5a27a6bf8c0fb745fac60da05bd
Jason Reeder <info@jasonreeder.com>
2017-03-20 11:20:37 -0700

    Prepare component to receive hash

--- frontend/components/messages/message_history.jsx
+++ frontend/components/messages/message_history.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const singleMessage = ({content, sender, time}) => {
+const singleMessage = ({body, sender, time}, idx) => {
 
   return(
-    <div className="single-message-container">
+    <div key={idx} className="single-message-container">
       <div className="single-message-header">
         <div className="sender">
           {sender}
@@ -13,36 +13,43 @@ const singleMessage = ({content, sender, time}) => {
         </div>
       </div>
       <div className="single-message-content">
-        {content}
+        {body}
       </div>
     </div>
   );
 };
 
 const MessageHistory = () => {
+  const messageHistory = [
+    {
+      body: "Mr. Watson",
+      sender: "edison",
+      time: "8:00 AM"
+    },
+    {
+      body: "Come here",
+      sender: "edison",
+      time: "8:01 AM"
+    },
+    {
+      body: "I want you",
+      sender: "edison",
+      time: "8:03 AM"
+    },
+    {
+        body: "He led no armies into battle, he conquered no countries, and he enslaved no peoples... Nonetheless, he exerted a degree of power the magnitude of which no warrior ever dreamed. His name still commands a respect as sweeping in scope and as world-wide as that of any other mortal - a devotion rooted deep in human gratitude and untainted by the bias that is often associated with race, color, politics, and religion.",
+        sender: "edison",
+        time: "8:05 AM"
+      },
+  ];
 
   return(
     <div className="message-history-container">
-      {singleMessage({
-         content: "Mr. Watson",
-         sender: "edison",
-         time: "8:00 AM"
-       })}
-      {singleMessage({
-         content: "Come here",
-         sender: "edison",
-         time: "8:01 AM"
-       })}
-      {singleMessage({
-         content: "I want you",
-         sender: "edison",
-         time: "8:03 AM"
-       })}
-      {singleMessage({
-         content: "He led no armies into battle, he conquered no countries, and he enslaved no peoples... Nonetheless, he exerted a degree of power the magnitude of which no warrior ever dreamed. His name still commands a respect as sweeping in scope and as world-wide as that of any other mortal - a devotion rooted deep in human gratitude and untainted by the bias that is often associated with race, color, politics, and religion.",
-         sender: "edison",
-         time: "8:05 AM"
-       })}
+      {
+        messageHistory.map((message, idx) => (
+          singleMessage(message, idx)
+        ))
+      }
     </div>
   );
 };
