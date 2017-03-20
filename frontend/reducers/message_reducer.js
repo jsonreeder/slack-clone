f1978f1bc7ee71f444f63134daa71fd40dca27e8
Jason Reeder <info@jasonreeder.com>
2017-03-20 15:26:25 -0700

    Add actions and reducer

---
+++ frontend/reducers/message_reducer.js
@@ -0,0 +1,4 @@
+import {
+  RECEIVE_ALL_FORUMS,
+  RECEIVE_SINGLE_FORUM
+} from '../actions/forum_actions';
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import ForumReducer from './forum_reducer';
 import UserReducer from './user_reducer';
+import MessageReducer from './messages_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   forum: ForumReducer,
-  users: UserReducer
+  users: UserReducer,
+  messages: MessageReducer
 });
 
 export default RootReducer;
