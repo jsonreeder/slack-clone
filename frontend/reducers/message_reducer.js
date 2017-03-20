3a9dffbb020729d6f4df5819abdb0307a341cea8
Jason Reeder <info@jasonreeder.com>
2017-03-20 16:47:28 -0700

    Remove unnecessary reducer

--- frontend/reducers/message_reducer.js
+++
@@ -1,4 +0,0 @@
-import {
-  RECEIVE_ALL_FORUMS,
-  RECEIVE_SINGLE_FORUM
-} from '../actions/forum_actions';
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,13 +3,11 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import ForumReducer from './forum_reducer';
 import UserReducer from './user_reducer';
-import MessageReducer from './messages_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   forum: ForumReducer,
-  users: UserReducer,
-  messages: MessageReducer
+  users: UserReducer
 });
 
 export default RootReducer;
