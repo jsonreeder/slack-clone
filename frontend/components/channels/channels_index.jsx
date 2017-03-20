11b2495d671f584e84b25d993c29db5d3731398d
Jason Reeder <info@jasonreeder.com>
2017-03-19 18:14:27 -0700

    Start styling

--- frontend/components/channels/channels_index.jsx
+++ frontend/components/channels/channels_index.jsx
@@ -7,12 +7,38 @@ class ChannelsIndex extends React.Component {
 
   componentDidMount() {
     this.props.requestAllForums();
+    $('html,body').css('overflow','hidden');
+  }
+
+  channelsIndexHeader() {
+    return(
+      <div className="channels-index-header">
+        <h1>Browse all channels</h1>
+      </div>
+    )
+  }
+
+  channelsIndexBody() {
+    return(
+      <div className="channels-index-body">
+        <h2>
+          Channels you can join
+        </h2>
+        <ul>
+          <li>A channel</li>
+          <li>Another channel</li>
+        </ul>
+      </div>
+    );
   }
 
   render() {
     return(
-      <div>
-        <h1>Browse all channels</h1>
+      <div className="channels-index-container">
+        <div className="channels-index-selector-container">
+          {this.channelsIndexHeader()}
+          {this.channelsIndexBody()}
+        </div>
       </div>
     );
   }
