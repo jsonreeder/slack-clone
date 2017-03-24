73751b9bfa35449d29a56d7b1954679898ca2a8d
Jason Reeder <info@jasonreeder.com>
2017-03-23 19:07:49 -0700

    Align messages to bottom

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -30,6 +30,10 @@ class Forum < ApplicationRecord
     Forum.where(kind: 'direct_message')
   end
 
+  def reverse_chron_messages
+    messages.order(created_at: :desc)
+  end
+
   def configure_dm(current_user, other_users)
     all_users = [current_user] + other_users
     all_users.sort!
