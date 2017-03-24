c312e0d6abfdb27a023a6b70f87fcf8e3841aeb9
Jason Reeder <info@jasonreeder.com>
2017-03-24 11:38:39 -0700

    Add print statements

--- app/controllers/api/forums_controller.rb
+++ app/controllers/api/forums_controller.rb
@@ -19,19 +19,40 @@ class Api::ForumsController < ApplicationController
     end
 
     @forum = Forum.new
+    print "-" * 20
+    print "DEBUGGING: @forum after initialization"
+    p @forum
+    print "-" * 20
+
     @forum.configure_dm(forum_params[:current_user], forum_params[:other_users])
 
+    print "-" * 20
+    print "DEBUGGING: @forum after configure_dm, before save"
+    p @forum
+    print "-" * 20
     if @forum.save
       current_user.forums << @forum
       other_users.each do |user|
         user.forums << @forum
       end
+      print "-" * 20
+      print "DEBUGGING: @forum after successful save"
+      p @forum
+      print "-" * 20
     else
       # Find the existing forum with that name
       new_forum = Forum.find_by(name: @forum.name)
       @forum = new_forum
+      print "-" * 20
+      print "DEBUGGING: @forum after unsuccessful save (the newly found forum)"
+      p @forum
+      print "-" * 20
     end
 
+    print "-" * 20
+    print "DEBUGGING: @forum before rendering api/forums/show in forums_controller"
+    p @forum
+    print "-" * 20
     render 'api/forums/show'
   end
 
